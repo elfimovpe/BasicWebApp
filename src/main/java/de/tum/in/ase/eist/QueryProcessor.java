7c2771f9f36bd2176f76da9512979061a738a361
elfimov <ge45laz@tum.de>
2022-07-14 13:05:02 +0200

    -

--- src/main/java/de/tum/in/ase/eist/QueryProcessor.java
+++ src/main/java/de/tum/in/ase/eist/QueryProcessor.java
@@ -13,8 +13,10 @@ public class QueryProcessor {
                     "writer in the English language and the world's pre-eminent dramatist.";
         } else if (query.contains("name")) {
            return "Peter";
-        } else { // TODO extend the programm here
+        } else if (query.contains("plus")){ // TODO extend the programm here
 
+            return "4034";
+        } else {
             return "";
         }
     }
